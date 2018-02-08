6a72eeaaac1600e7893ad53fa315273c6c43094c
abegus <agbues@gmail.com>
2018-02-08 13:10:05 -0600

    Adding NetworkViewModel

---
+++ 744Project/Controllers/NetworkController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _744Project.Models;
+
+namespace _744Project.Controllers
+{
+    public class NetworkController : Controller
+    {
+        private MasterModel db = new MasterModel();
+
+        // GET: Network
+        public ActionResult Index()
+        {
+
+            return View(db.Relays.ToList());
+        }
+
+        // GET: Network/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Relay relay = db.Relays.Find(id);
+            if (relay == null)
+            {
+                return HttpNotFound();
+            }
+            return View(relay);
+        }
+
+        // GET: Network/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Network/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "relayID,relayName")] Relay relay)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Relays.Add(relay);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(relay);
+        }
+
+        // GET: Network/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Relay relay = db.Relays.Find(id);
+            if (relay == null)
+            {
+                return HttpNotFound();
+            }
+            return View(relay);
+        }
+
+        // POST: Network/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "relayID,relayName")] Relay relay)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(relay).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(relay);
+        }
+
+        // GET: Network/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Relay relay = db.Relays.Find(id);
+            if (relay == null)
+            {
+                return HttpNotFound();
+            }
+            return View(relay);
+        }
+
+        // POST: Network/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Relay relay = db.Relays.Find(id);
+            db.Relays.Remove(relay);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 744Project/ViewModels/NetworkViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using _744Project.Models;
+using System.Collections;
+
+
+namespace _744Project.ViewModels
+{
+
+    public class NetworkViewModel
+    {
+        private MasterModel db = new MasterModel();
+
+        public List<IpConnection> connections; // a list of the connections between NetworkEntities    (connections)
+        public Dictionary<String, NetworkEntity> networkEntities; // a list of data objects from the Network.  (store, relay, PC)
+
+        public NetworkViewModel()
+        {
+            var storeToRelays = from store in db.Stores select store;
+            connections = convertStoresToIpConnections(storeToRelays);
+
+            var relayToRelays = from rr in db.RelayToRelayConnections select rr;
+            connections.AddRange(convertRelayConnections(relayToRelays));
+
+            var relayToPC= from rpc in db.RelayToProcessCenterConnections select rpc;
+            connections.AddRange(convertRelayProcessingConnections(relayToPC));
+        }
+
+        private List<IpConnection> convertStoresToIpConnections(IEnumerable<Store> stores)
+        {
+            List<IpConnection> newConnections = new List<IpConnection>();
+
+            //convert stores to connections...
+
+            return newConnections;
+        }
+
+        private List<IpConnection> convertRelayConnections(IEnumerable<RelayToRelayConnection> rr)
+        {
+            List<IpConnection> newConnections = new List<IpConnection>();
+
+            //convert relay-relay to connections...
+
+            return newConnections;
+        }
+
+        private List<IpConnection> convertRelayProcessingConnections(IEnumerable<RelayToProcessCenterConnection> rpc)
+        {
+            List<IpConnection> newConnections = new List<IpConnection>();
+
+            //convert relay-relay to connections...
+
+            return newConnections;
+        }
+
+
+    }
+
+    public class IpConnection
+    {
+
+    }
+
+    public class NetworkEntity
+    {
+
+    }
+}
