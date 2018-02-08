9a27e11267a0475fb51755a5abb12b2f831e4a3c
alsyefs <Saleh.Alsyefi@gmail.com>
2018-02-08 10:51:14 -0600

    Added Accounts, CreditCards, and Cutomers Views & Controllers. The tabs have also been added for the authenticated user.

---
+++ 744Project/Controllers/CreditCardsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using _744Project.Models;
+
+namespace _744Project.Controllers
+{
+    public class CreditCardsController : Controller
+    {
+        private MasterModel db = new MasterModel();
+
+        // GET: CreditCards
+        public ActionResult Index()
+        {
+            var creditCards = db.CreditCards.Include(c => c.Account).Include(c => c.Customer);
+            return View(creditCards.ToList());
+        }
+
+        // GET: CreditCards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Create
+        public ActionResult Create()
+        {
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountNumber");
+            ViewBag.customerID = new SelectList(db.Customers, "customerID", "customerFirstname");
+            return View();
+        }
+
+        // POST: CreditCards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cardID,cardNumber,cardExpirationDate,cardSecurityCode,cardMaxAllowed,accountID,customerID")] CreditCard creditCard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CreditCards.Add(creditCard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountNumber", creditCard.accountID);
+            ViewBag.customerID = new SelectList(db.Customers, "customerID", "customerFirstname", creditCard.customerID);
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountNumber", creditCard.accountID);
+            ViewBag.customerID = new SelectList(db.Customers, "customerID", "customerFirstname", creditCard.customerID);
+            return View(creditCard);
+        }
+
+        // POST: CreditCards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cardID,cardNumber,cardExpirationDate,cardSecurityCode,cardMaxAllowed,accountID,customerID")] CreditCard creditCard)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(creditCard).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.accountID = new SelectList(db.Accounts, "accountID", "accountNumber", creditCard.accountID);
+            ViewBag.customerID = new SelectList(db.Customers, "customerID", "customerFirstname", creditCard.customerID);
+            return View(creditCard);
+        }
+
+        // GET: CreditCards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CreditCard creditCard = db.CreditCards.Find(id);
+            if (creditCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(creditCard);
+        }
+
+        // POST: CreditCards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CreditCard creditCard = db.CreditCards.Find(id);
+            db.CreditCards.Remove(creditCard);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
