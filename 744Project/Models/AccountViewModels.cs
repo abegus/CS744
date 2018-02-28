17a58c31dbdd0712bd3e5550155aaf2bd6766bcb
abegus <agbues@gmail.com>
2018-02-28 10:44:44 -0600

    Updated Email to Username. Still issues with register because the Security Questions aren't working

--- 744Project/Models/AccountViewModels.cs
+++ 744Project/Models/AccountViewModels.cs
@@ -50,8 +50,8 @@ namespace _744Project.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
+        [Display(Name = "Username")]
+       // [EmailAddress]
         public string Email { get; set; }
 
         [Required]
@@ -76,8 +76,8 @@ namespace _744Project.Models
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
+        //[EmailAddress]
+        [Display(Name = "Username")]
         public string Email { get; set; }
 
         [Required]
