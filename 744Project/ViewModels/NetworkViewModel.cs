03baba12bec54d5482782fb584ffd3181ac65741
abegus <agbues@gmail.com>
2018-03-19 13:32:05 -0500

    Updating network view model to have active/inactive / isgateway tags

--- 744Project/ViewModels/NetworkViewModel.cs
+++ 744Project/ViewModels/NetworkViewModel.cs
@@ -59,13 +59,13 @@ namespace _744Project.ViewModels
             foreach(var store in stores)
             {
                 //add a new connection from store to relay
-                connections.Add(new IpConnection(store.storeIP, store.Relay.relayIP, store.storeWeight));
+                connections.Add(new IpConnection(store.storeIP, store.Relay.relayIP, store.storeWeight, false));//don't have relay to store connections inactivatable in database yet.
 
                 //get locations
                 var location1 = getEntityLocation(store.storeIP);
 
                 //add JUST THE STORE to the network entities
-                NetworkEntity temp = new NetworkEntity(store.storeIP, 0, store.storeID, location1.Item1, location1.Item2);
+                NetworkEntity temp = new NetworkEntity(store.storeIP, 0, store.storeID, location1.Item1, location1.Item2, true, false);
                 networkEntities.Add(store.storeIP, temp);
 
                 //only add it if it doesnt already exist
@@ -73,7 +73,7 @@ namespace _744Project.ViewModels
                 {
                     var location2 = getEntityLocation(store.Relay.relayIP);
 
-                    NetworkEntity relay = new NetworkEntity(store.Relay.relayIP, 1, store.Relay.relayID, location2.Item1, location2.Item2);
+                    NetworkEntity relay = new NetworkEntity(store.Relay.relayIP, 1, store.Relay.relayID, location2.Item1, location2.Item2, store.Relay.isActive, store.Relay.isGateway);
                     networkEntities.Add(store.Relay.relayIP, relay);
                 }
                
@@ -89,14 +89,14 @@ namespace _744Project.ViewModels
             //convert relay-relay to connections...
             foreach(var relayCon in rr)
             {
-                connections.Add(new IpConnection(relayCon.Relay.relayIP, relayCon.Relay2.relayIP, relayCon.relayWeight));
+                connections.Add(new IpConnection(relayCon.Relay.relayIP, relayCon.Relay2.relayIP, relayCon.relayWeight, relayCon.isActive));
 
                 //get locations
                 var location1 = getEntityLocation(relayCon.Relay.relayIP);
                 var location2 = getEntityLocation(relayCon.Relay2.relayIP);
 
-                NetworkEntity temp1 = new NetworkEntity(relayCon.Relay.relayIP, 1, relayCon.Relay.relayID, location1.Item1, location1.Item2);
-                NetworkEntity temp2 = new NetworkEntity(relayCon.Relay2.relayIP, 1, relayCon.Relay2.relayID, location2.Item1, location2.Item2);
+                NetworkEntity temp1 = new NetworkEntity(relayCon.Relay.relayIP, 1, relayCon.Relay.relayID, location1.Item1, location1.Item2, relayCon.Relay.isActive, relayCon.Relay.isGateway);
+                NetworkEntity temp2 = new NetworkEntity(relayCon.Relay2.relayIP, 1, relayCon.Relay2.relayID, location2.Item1, location2.Item2, relayCon.Relay2.isActive, relayCon.Relay2.isGateway);
                 if (!networkEntities.ContainsKey(relayCon.Relay.relayIP)) 
                     networkEntities.Add(relayCon.Relay.relayIP, temp1 );
                 if (!networkEntities.ContainsKey(relayCon.Relay2.relayIP))
@@ -113,14 +113,14 @@ namespace _744Project.ViewModels
             //convert relay-PC to connections...
             foreach(var con in rpc)
             {
-                connections.Add(new IpConnection(con.Relay.relayIP, con.ProcessCenter.processCenterIP, con.relayToProcessCenterConnectionWeight));
+                connections.Add(new IpConnection(con.Relay.relayIP, con.ProcessCenter.processCenterIP, con.relayToProcessCenterConnectionWeight, con.isActive)); // not in database yet
 
                 //Get locations
                 var location1 = getEntityLocation(con.Relay.relayIP);
                 var location2 = getEntityLocation(con.ProcessCenter.processCenterIP);
 
-                NetworkEntity temp1 = new NetworkEntity(con.Relay.relayIP, 1, con.Relay.relayID,location1.Item1, location1.Item2);
-                NetworkEntity temp2 = new NetworkEntity(con.ProcessCenter.processCenterIP, 2, con.ProcessCenter.processCenterID, location2.Item1, location2.Item2);
+                NetworkEntity temp1 = new NetworkEntity(con.Relay.relayIP, 1, con.Relay.relayID,location1.Item1, location1.Item2, con.Relay.isActive, con.Relay.isGateway);
+                NetworkEntity temp2 = new NetworkEntity(con.ProcessCenter.processCenterIP, 2, con.ProcessCenter.processCenterID, location2.Item1, location2.Item2, con.Relay.isActive, con.Relay.isGateway);
                 if (!networkEntities.ContainsKey(con.Relay.relayIP))
                     networkEntities.Add(con.Relay.relayIP, temp1);
                 if (!networkEntities.ContainsKey(con.ProcessCenter.processCenterIP))
@@ -170,9 +170,11 @@ namespace _744Project.ViewModels
         public string ip1 { get; set; }
         public string ip2 { get; set; }
         public int weight { get; set; }
+        public bool isActive { get; set; }
 
-        public IpConnection (string ip1, string ip2, int? weight)
+        public IpConnection (string ip1, string ip2, int? weight, bool isActive)
         {
+            this.isActive = isActive;
             this.ip1 = ip1;
             this.ip2 = ip2;
             this.weight = weight??0;
@@ -184,18 +186,22 @@ namespace _744Project.ViewModels
         public string ip { get; set; }
         public int type { get; set; }//0 store,   1 relay,    2 PC
         public string databaseId { get; set; }
+        public bool isActive { get; set; }
+        public bool isGateway { get; set; }
 
         //for view locations
         public decimal x { get; set; }
         public decimal y { get; set; }
 
-        public NetworkEntity(string ip, int type, string databaseId, decimal x, decimal y)
+        public NetworkEntity(string ip, int type, string databaseId, decimal x, decimal y, bool isActive, bool isGateway)
         {
             this.ip = ip;
             this.type = type;
             this.databaseId = databaseId;
             this.x = x;
             this.y = y;
+            this.isActive = isActive;
+            this.isGateway = isGateway;
         }
         //maybe a list<Transactions>..... numTransactions....
     }
