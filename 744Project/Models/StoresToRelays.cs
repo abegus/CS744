0e9a8c530618ac8b5d835dc3d24bdd01b9d36dbc
alsyefs <Saleh.Alsyefi@gmail.com>
2018-03-21 18:36:51 -0500

    Changed the DB: 1. Added many-to-many relationship between stores and relays. 2. Everything has been tested with the server's DB and it functions well. 3. Added the data manually in the table StoresToRelays.

---
+++ 744Project/Models/StoresToRelays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _744Project.Models
+{
+    public class StoresToRelays
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public StoresToRelays()
+        {
+          
+        }
+
+        [Key]
+        public int storesToRelaysID { get; set; }
+        
+
+        [StringLength(50)]
+        public string relayID { get; set; }
+        public virtual Relay Relay { get; set; }
+
+        [StringLength(50)]
+        public string storeID { get; set; }
+        public virtual Store Store { get; set; }
+        
+
+    }
+}
